dbef22d971d5c7de9f7c1132878edab35c7d9b07
cet <309521305@qq.com>
2022-10-23 19:42:41 +0800

    修改部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,99 @@
-nohup java -jar target/sql-father-backend.jar &
+#! /bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.292.b10-1.el7_9.x86_64
+export JRE_HOME=$JAVA_HOME/jre
+
+API_NAME=publicize-manage
+JAR_NAME=/home/admin/application/target/$API_NAME\.jar
+LOG=/home/admin/application/$API_NAME\.out
+#PID  代表是PID文件
+PID=/home/admin/application/$API_NAME\.pid
+
+#使用说明，用来提示输入参数
+usage() {
+    echo "Usage: sh 执行脚本.sh [start|stop|restart|status]"
+    exit 1
+}
+
+#检查程序是否在运行
+is_exist(){
+  pid=`ps -ef|grep $JAR_NAME|grep -v grep|awk '{print $2}' `
+  #如果不存在返回1，存在返回0
+  if [ -z "${pid}" ]; then
+   return 1
+  else
+    return 0
+  fi
+}
+
+#启动方法
+start(){
+  is_exist
+  if [ $? -eq "0" ]; then
+    echo ">>> ${JAR_NAME} is already running PID=${pid} <<<"
+  else
+    nohup $JRE_HOME/bin/java -Xms256m -Xmx512m -jar $JAR_NAME >$LOG 2>&1 &
+    echo $! > $PID
+    echo ">>> start $JAR_NAME successed PID=$! <<<"
+   fi
+  }
+
+#停止方法
+stop(){
+  #is_exist
+  pidf=$(cat $PID)
+  #echo "$pidf"
+  echo ">>> ${API_NAME} PID = $pidf begin kill $pidf <<<"
+  kill $pidf
+  rm -rf $PID
+  sleep 2
+  is_exist
+  if [ $? -eq "0" ]; then
+    echo ">>> ${API_NAME} 2 PID = $pid begin kill -9 $pid  <<<"
+    kill -9  $pid
+    sleep 2
+    echo ">>> $JAR_NAME process stopped <<<"
+  else
+    echo ">>> ${JAR_NAME} is not running <<<"
+  fi
+}
+
+#输出运行状态
+status(){
+  is_exist
+  if [ $? -eq "0" ]; then
+    echo ">>> ${JAR_NAME} is running PID is ${pid} <<<"
+  else
+    echo ">>> ${JAR_NAME} is not running <<<"
+  fi
+}
+
+#重启
+restart(){
+  stop
+  start
+}
+
+#根据输入参数，选择执行对应方法，不输入则执行使用说明
+case "$1" in
+  "start")
+    start
+    ;;
+  "stop")
+    stop
+    ;;
+  "status")
+    status
+    ;;
+  "restart")
+    restart
+    ;;
+  *)
+    usage
+    ;;
+esac
+exit 0
+
+
+
+
+
